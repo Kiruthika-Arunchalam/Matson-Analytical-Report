c22138daea270791b5f7714acc8d824be228e1be
Kiruthika-Arunchalam <kiruthika.arun19@gmail.com>
2026-02-13 21:56:30 +0530

    Replace dark_plot with apply_strict_dark_theme

--- app.py
+++ app.py
@@ -404,7 +404,7 @@ if vv_list:
         )
 
         fig_g.update_yaxes(autorange="reversed")
-        fig = dark_plot(fig)
+        fig = apply_strict_dark_theme(fig)
 
         st.plotly_chart(fig_g, use_container_width=True)
 
